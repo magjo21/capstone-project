e64b2202b3f4d914f051bf72af556a04b3cb1b83
Magnus Johannson <magnus.johannson@gmx.de>
2023-01-19 12:55:38 +0100

    add buttons

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,16 @@
 export default function HomePage() {
-  return <h1>Hello from Next.js</h1>;
+  return (
+    <>
+      <h1>EasyRev</h1>
+      <button type="button" onClick={() => ()}>
+        Good
+      </button>
+      <button type="button" onClick={() => ()}>
+        Middle
+      </button>
+      <button type="button" onClick={() => ()}>
+        Bad
+      </button>
+    </>
+  );
 }
